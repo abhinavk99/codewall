0d18a444720c6501620a92bbacdde3048f3fc9b0
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2021-01-08 02:21:14 -0600

    Fix dealing with ruler objects from settings and refactor code (#5)

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,17 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as codewall from '../../extension';
 
-suite('Extension Test Suite', () => {
-  vscode.window.showInformationMessage('Start all tests.');
+suite('CodeWall Test Suite', () => {
+  vscode.window.showInformationMessage('Start all CodeWall tests.');
+  const codeWall = new codewall.CodeWall();
 
-  test('Sample test', () => {
-    assert.equal([1, 2, 3].indexOf(5), -1);
-    assert.equal([1, 2, 3].indexOf(0), -1);
+  test('Get ruler number from number', () => {
+    assert.strictEqual(codeWall.getRulerNumber(5), 5);
+  });
+
+  test('Get ruler number from object', () => {
+    assert.strictEqual(codeWall.getRulerNumber({ column: 5, color: '#000000' }), 5);
   });
 });
