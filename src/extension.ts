cf81852e5e7de84f590723feae5ca850ac7fec8b
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2021-01-08 02:21:14 -0600

    Fix dealing with ruler objects from settings and refactor code (#5)

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,16 @@
 
 import * as vscode from 'vscode';
 
+// A ruler in VS Code settings can either be a number or an object mapping strings to numbers/strings
+// e.g. 90
+// e.g. {"column": 90, "color": "#000"}
+type Ruler = number | RulerObject;
+
+interface RulerObject {
+  column: number;
+  color: string;
+}
+
 export function activate(context: vscode.ExtensionContext): void {
   const diagnostics = vscode.languages.createDiagnosticCollection('CodeWall');
   const codeWall = new CodeWall();
@@ -31,31 +41,33 @@ export function deactivate(): void {
   return;
 }
 
-class CodeWall {
-  public checkCrossingWall(document: vscode.TextDocument, diagnosticCollection: vscode.DiagnosticCollection) {
+export class CodeWall {
+  /**
+   * Goes through the document and adds warnings for any lines crossing any rulers.
+   * @param document VS Code text document
+   * @param diagnosticCollection Diagnostics collection to add warnings to
+   */
+  public checkCrossingWall(document: vscode.TextDocument, diagnosticCollection: vscode.DiagnosticCollection): void {
     diagnosticCollection.clear();
     const diagnostics: vscode.Diagnostic[] = [];
 
-    // Get rulers in descending order
-    const rulers: Array<number | Record<string, unknown>> = vscode.workspace
-      .getConfiguration('editor')
-      .get('rulers', []);
-    rulers.sort((a, b) => b - a);
+    const rulers = this.getRulersDescendingOrder();
+    if (rulers.length == 0) {
+      return;
+    }
 
-    // Go through all the lines in the document
-    const lineCount = document.lineCount;
-    for (let lineNumber = 0; lineNumber < lineCount; lineNumber++) {
-      const line = document.lineAt(lineNumber);
-      const character = line.range.end.character;
+    for (const line of this.getLines(document)) {
       for (const ruler of rulers) {
-        // Check if the line passes the ruler
-        if (character > ruler) {
-          const message = `Line ${lineNumber + 1} is longer than ruler at ${ruler}.`;
-          console.log(message);
+        if (this.doesLinePassRuler(line, ruler)) {
+          const message = `Line ${line.lineNumber + 1} is longer than ruler at ${this.getRulerNumber(ruler)}.`;
+          // Add VS Code warning
           diagnostics.push({
             code: '',
             message: message,
-            range: new vscode.Range(new vscode.Position(lineNumber, ruler - 1), line.range.end),
+            range: new vscode.Range(
+              new vscode.Position(line.lineNumber, this.getRulerNumber(ruler) - 1),
+              line.range.end
+            ),
             severity: vscode.DiagnosticSeverity.Warning,
           });
           break;
@@ -73,7 +85,51 @@ class CodeWall {
     }
   }
 
-  public dispose() {
+  /**
+   * Gets all the rulers in the VS Code settings in descending order.
+   */
+  private getRulersDescendingOrder() {
+    const rulers: Array<Ruler> = vscode.workspace.getConfiguration('editor').get('rulers', []);
+    rulers.sort((a, b) => this.getRulerNumber(b) - this.getRulerNumber(a)); // Descending order
+    return rulers;
+  }
+
+  /**
+   * Since a ruler can either be a number or an object, this method abstracts out getting
+   * the line number from the ruler.
+   * @param ruler VS Code ruler from settings
+   * @returns Ruler line number
+   */
+  public getRulerNumber(ruler: Ruler): number {
+    if (typeof ruler === 'number') {
+      return ruler;
+    }
+    return ruler.column;
+  }
+
+  /**
+   * Gets all the lines in the text document as an iterator.
+   * @param document VS Code text document
+   * @returns Iterator of lines in the text document
+   */
+  private *getLines(document: vscode.TextDocument) {
+    const lineCount = document.lineCount;
+    for (let lineNumber = 0; lineNumber < lineCount; lineNumber++) {
+      yield document.lineAt(lineNumber);
+    }
+  }
+
+  /**
+   * Returns whether the line passes the ruler.
+   * @param line Position number of the last character in the line.
+   * @param ruler Ruler being checked
+   * @returns True if line passes the ruler, otherwise false
+   */
+  private doesLinePassRuler(line: vscode.TextLine, ruler: Ruler) {
+    return line.range.end.character > this.getRulerNumber(ruler);
+  }
+
+  public dispose(): void {
     return;
   }
 }
